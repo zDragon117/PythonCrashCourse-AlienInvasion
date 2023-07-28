e5786aa503704fa894562081d1ce220c9ce7f9ff
Zet Truong <dungtq@aipower.com.vn>
2023-07-28 18:54:04 +0700

    Update Aliens. Add button Play.

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 640
+        self.screen_height = 480
         self.bg_color = (230, 230, 230)
 
         # Ship settings
@@ -21,6 +21,6 @@ class Settings:
 
         # Alien settings
         self.alien_speed_factor = .1
-        self.fleet_drop_speed = 5
+        self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
